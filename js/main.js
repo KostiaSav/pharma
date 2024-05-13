c83814c0e3d647349de1acad39971e8769b140ee
Kostya Prodan <debaun4ry@gmail.com>
2024-05-13 23:15:43 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,102 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/_components.js":
+/*!*******************************!*\
+  !*** ./src/js/_components.js ***!
+  \*******************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_animates_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/animates.js */ "./src/js/components/animates.js");
+
+
+/***/ }),
+
+/***/ "./src/js/components/animates.js":
+/*!***************************************!*\
+  !*** ./src/js/components/animates.js ***!
+  \***************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+let buttons = document.getElementsByClassName("btn-blue");
+const buttonAnimate = e => {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  e.target.style.setProperty('--mouse-x', x + "px");
+  e.target.style.setProperty('--mouse-y', y + "px");
+};
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("mousemove", buttonAnimate);
+}
+const ctaBottom = document.getElementsByClassName("cta-bottom");
+const ctaTop = document.getElementsByClassName("cta-top");
+let ctaBottomA = new simpleParallax(ctaBottom, {
+  orientation: 'right',
+  overflow: true,
+  scale: 1.4
+});
+let ctaTopA = new simpleParallax(ctaTop, {
+  orientation: 'left',
+  overflow: true,
+  scale: 1.8
+});
+console.log(ctaBottomA);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_components.js */ "./src/js/_components.js");
+
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
